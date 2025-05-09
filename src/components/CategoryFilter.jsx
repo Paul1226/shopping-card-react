8578dbc3469183004fb6cbd4b967a1526742fcea
Paul Guachichullca <paulguachichullca023@gmail.com>
2025-05-09 09:22:28 -0500

    feat: agregar selección de categoría en CategoryFilter y estilos para botón activo

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,26 +1,26 @@
 import { products as PRODUCTS } from "../data/products.json";
 
-export const CategoryFilter = ({ setCategorySelect }) => {
+export const CategoryFilter = ({ categorySelect, setCategorySelect }) => {
   const categories = [
     "Todas",
     ...new Set(PRODUCTS.map((product) => product.category)),
   ];
   return (
-    <div className="sticky top-16 z-10 bg-white header-page py-4">
-      <nav className="w-full flex flex-col gap-3">
-        <ul className="flex items-center gap-3 slider-scroll-x w-full max-w-7xl overflow-hidden overflow-x-auto pb-3">
-          {categories.map((category) => (
-            <li key={category} className="w-full">
-              <button
-                className="cursor-pointer bg-neutral-100 transition-colors duration-200 ease-in-out hover:bg-neutral-200 px-2 py-1 rounded-full scroll-"
-                onClick={() => setCategorySelect(category)}
-              >
-                {category}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
+    <nav className="w-full flex flex-col gap-3">
+      <ul className="flex items-center gap-3 slider-scroll-x w-full max-w-7xl overflow-hidden overflow-x-auto py-3">
+        {categories.map((category) => (
+          <li key={category} className="w-full">
+            <button
+              className={`${
+                categorySelect === category ? "active" : ""
+              } cursor-pointer bg-neutral-100 transition-all duration-300 ease-in-out hover:bg-neutral-200 px-2 py-1 rounded-full button-category`}
+              onClick={() => setCategorySelect(category)}
+            >
+              {category}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
--- src/components/ProductCatalog.jsx
+++ src/components/ProductCatalog.jsx
@@ -13,20 +13,25 @@ export const ProductCatalog = () => {
       : filteredProducts.filter((prod) => prod.category === categorySelect);
 
   return (
-    <div className="w-full max-w-6xl mx-auto py-8 px-5 flex flex-col gap-4">
+    <section className="w-full max-w-6xl mx-auto py-8 px-5 flex flex-col gap-6">
       <div className="flex justify-between items-center gap-2">
         <h2 className="text-3xl font-bold text-secondary text-balance">
           Para todas tus necesidades
         </h2>
         <ProductSearch />
       </div>
-      <p className="font-bold text-xl">Categorías</p>
-      <CategoryFilter setCategorySelect={setCategorySelect} />
+      <div className="sticky top-16 z-10 bg-white">
+        <p className="font-bold text-xl">Categorías</p>
+        <CategoryFilter
+          categorySelect={categorySelect}
+          setCategorySelect={setCategorySelect}
+        />
+      </div>
       <div className="container-list grid [grid-template-columns:repeat(auto-fill,minmax(200px,1fr))] gap-5">
         {filterProduct.map((product) => (
           <ProductCard key={product.id} {...product} />
         ))}
       </div>
-    </div>
+    </section>
   );
 };
